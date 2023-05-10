55667c54c6ab1503746025aadd9cde82434bdc52
Trishan Ghosh <ghoshtrishan73@gmail.com>
2023-05-10 11:59:55 +0530

    more changes

--- linked_list.js
+++ linked_list.js
@@ -6,53 +6,76 @@ class Node {
 }
 
 class SinglyLinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
-        this.tail = null;
-    }
+  constructor() {
+    this.head = null;
+    this.length = 0;
+    this.tail = null;
+  }
 
-    push(val) {
-        let newNode = new Node(val)
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode; 
-        }
-        this.length++;
-        return this;
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
 
-    pop() {
-        if(!this.head) return undefined;
-        let current = this.head;
-        let newTail = current;
-        // Traversing the List
-        while(current.next) { 
-            newTail = current;
-            current = current.next;
-        }
-        console.log('new tail', newTail);
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        if(this.length === 0) {
-            this.head = null;
-            this.tail = null;
-        }
-        return this;
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    // Traversing the List
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
-
-    shift() {
-        if(!this.head) return undefined;
-        let temp = this.head;
-        this.head = temp.next;
-        this.length--;
-        return this;
-
+    console.log("new tail", newTail);
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
     }
+    return this;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = temp.next;
+    this.length--;
+    return this;
+  }
+
+  unShift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if(index < 0 || index >= this.length) return null;
+    var counter = 0;
+    var current = this.head;
+    while(counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
 }
 
 var list = new SinglyLinkedList();
